c3fce50d78d00a0c2d6b3825573589ea72e69e97
rdunavant0 <rdunavant0@gmail.com>
2019-01-23 15:46:52 -0600

    message

---
+++ P1.py
@@ -0,0 +1,33 @@
+#Problem 1
+def problem1():
+    arrayForProblem2 = ["Kenn", "Kevin", "Erin", "Meka"]
+    print(arrayForProblem2[3])
+    print(arrayForProblem2[4])
+    list.remove("Kevin")
+    print(arrayForProblem2[3])
+
+#Problem 2
+def problem2():
+    userInput = ""
+    while(userInput != 'q'):
+        userInput = input("Enter something")
+
+#Problem 3
+def problem3():
+    names = {"Jonathan": "John", "Michael": "Mike", "William": "Bill", "Robert": "Rob"}
+    print(names)
+    print(names["Bill"])
+
+#Problem 4
+def problem4():
+    array = [2, 20, 200, 2000, 20000]
+    for array reversed
+        print(array)
+
+#Problem 5
+def problem5():
+    array = []
+    number = input("Enter a number")
+    userInput = numbers in array > number
+    userInput2 = numbers in array < number
+    userInput3 = numbers in array == number 
